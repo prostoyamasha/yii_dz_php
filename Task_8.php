6cd619987e23f708457975add0fb3590637a0cea
prostoyamasha/masha <ma.mi.morgun@gmail.com>
2017-03-23 23:12:24 +0200

    task_8

---
+++ Task_8.php
@@ -0,0 +1,36 @@
+<?php
+/*Task-8
+ *- выбрать все категории;
+ * SELECT * FROM `category`;
+ * 
+ *- выбрать категорию с айдишником 2;
+ * SELECT * FROM `category` WHERE 'id' = 2;
+ * 
+ *- выбрать категорию название которой 'CocaCola';
+ * SELECT * FROM `category` WHERE `title` = 'Coca-Cola';
+ * 
+ *- выбрать категорию название которой содержит 'Cola';
+ * SELECT * FROM `category` WHERE `title` LIKE 'Cola';
+ * 
+ *- выбрать все активные категории;
+ * SELECT * FROM `category` WHERE `status` = 1;
+ * 
+ *- выбрать все продукты с ценой большй одного доллара;
+ * SELECT * FROM `product` WHERE `price` > 1;
+ * 
+ *- выбрать все продукты с ценой большй одного доллара
+ *  но меньше трех долларов;
+ * SELECT * FROM `product` WHERE `price` > 1 AND 'price'> 3;
+ * 
+ *- выбрать все продукты категории кокакола;
+ * SELECT * FROM `product` WHERE `category_id' = 1;
+ * 
+ *-выбрать все продукты из всех активных категорий цена которых 
+ *  больше одного доллара и меньше трех в названии которых 
+ *  есть слово 'Cola'
+ * SELECT * FROM `product` WHERE 'category_id' = 1 AND 'price' > 1'price'> 3 
+ * `title` LIKE 'Cola';
+ */
+
+
+
--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=yii2.loc;dbname=yii2',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2',
     'username' => 'root',
     'password' => '111111',
     'charset' => 'utf8',
