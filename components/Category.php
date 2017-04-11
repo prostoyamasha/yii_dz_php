6ef8cdd3d3ec5183050bb746cba61304c5682944
prostoyamasha/masha <ma.mi.morgun@gmail.com>
2017-04-11 13:17:35 +0300

    selectMenu

---
+++ components/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Component;
+
+class Category extends Component
+{
+
+    public function getStatuses() {
+    return [0=>'не активная',
+        1=>'активная'];
+    }
+}
--- views/category/create.php
+++ views/category/create.php
@@ -2,11 +2,16 @@
 
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
+use app\components\Category;
+
 ?>
 <h1>Create Category</h1>
 <?php $form = ActiveForm::begin(); ?>
 
 <?php echo $form->field($model, 'title') ?>
+
+<?php echo Html::dropDownList('status','1', Category::getStatuses()); ?>
+
 <?php echo Html::submitButton('Create',['class'=>'btn btn-primary']); ?>
 
 <?php ActiveForm::end(); ?>
