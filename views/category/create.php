9b9503be60943bbd838fb81c8169be8571c18927
Vladimir Teplov <v.teplov@mobidev.biz>
2017-04-12 00:16:56 +0300

    category create action changes

--- views/category/create.php
+++ views/category/create.php
@@ -7,11 +7,13 @@ use app\components\Category;
 ?>
 <h1>Create Category</h1>
 <?php $form = ActiveForm::begin(); ?>
-
-<?php echo $form->field($model, 'title') ?>
-
-<?php echo Html::dropDownList('status','1', Category::getStatuses()); ?>
-
-<?php echo Html::submitButton('Create',['class'=>'btn btn-primary']); ?>
-
+<div>
+    <?php echo $form->field($model, 'title'); ?>
+</div>
+<div>
+    <?php echo $form->field($model, 'status')->dropDownList(Category::getStatuses()); ?>
+</div>
+<div>
+    <?php echo Html::submitButton('Create',['class'=>'btn btn-primary']); ?>
+</div>
 <?php ActiveForm::end(); ?>
